aa67b39fb0f2a26594fbb3bb40c6658f14a53389
Mora <qiuzhongleiabc@126.com>
2019-05-22 17:27:21 +0800

    feat: 提供 antd-icon

---
+++ projects/serpent-react-admin-kits/build/get-antd-icon.js
@@ -0,0 +1,32 @@
+/* eslint "@typescript-eslint/no-var-requires": "off" */
+const fs = require('fs')
+const path = require('path')
+const typesDir = path.resolve(__dirname, '..')
+
+/**
+ * 解析 node_modules/@ant-design/icons/svg/ 中的 svg 名称
+ * 生成 res/generate/types/antd-icon.d.ts 文件
+ */
+function getAntdIcon() {
+  const target = 'node_modules/@ant-design/icons/svg/'
+  const folder = path.resolve(__dirname, '..', ...target.split('/'))
+  /** @type {string[]} */
+  const result = []
+  let content = `// prettier-ignore\ndeclare type ANTD_ICON =`
+  fs.readdirSync(folder).forEach(name => {
+    if (name.startsWith('.')) return
+    const dir = path.join(folder, name)
+    result.push(
+      ...fs
+        .readdirSync(dir)
+        .filter(n => n.endsWith('.svg'))
+        .map(n => `${n.replace(/\.svg$/, '')}`)
+    )
+  })
+
+  content += result.map(n => `'${n}'`).join('\n  | ') + '\n'
+  console.log('generate ' + path.join(typesDir, 'antd-icon.d.ts'))
+  fs.writeFileSync(path.join(typesDir, 'antd-icon.d.ts'), content)
+}
+
+getAntdIcon()
