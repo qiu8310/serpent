48c5520914557c52622315b4b3d6889503d5a998
Mora <qiuzhongleiabc@126.com>
2019-07-19 17:03:00 +0800

    peerDependencies 更宽容

--- projects/serpent-cli/src/serpent-peer.ts
+++ projects/serpent-cli/src/serpent-peer.ts
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import path from 'path'
 
-const PROJECTS_ROOT = path.resolve(__dirname, '..', '..')
+// const PROJECTS_ROOT = path.resolve(__dirname, '..', '..')
 
 interface D {
   [key: string]: string
@@ -17,7 +17,9 @@ export function peer(rootDir: string) {
   const dependencies = deep(pkg.dependencies)
   const peerDependencies: D = {}
   for (const [key, value] of Object.entries(dependencies)) {
-    peerDependencies[key] = value.replace(/^(\d+\.)/, '^$1')
+    // 版本的第一位前添加 ^
+    // 版本的最后一位修改成 0 # 第三方有可能不会那么快更新到最新的模块
+    peerDependencies[key] = value.replace(/^(\d+\.)/, '^$1').replace(/\.(\d+)$/, '.0')
   }
 
   if (JSON.stringify(peerDependencies) !== JSON.stringify(pkg.peerDependencies)) {
@@ -30,8 +32,9 @@ function deep(dependencies: D) {
   let res: D = { ...dependencies }
   for (const [key] of Object.entries(dependencies)) {
     if (key.startsWith('@serpent/')) {
-      const pkg = require(path.join(PROJECTS_ROOT, key.substr(1).replace('/', '-'), 'package.json'))
-      res = { ...deep(pkg.dependencies || {}), ...res }
+      // 不需要继续解析，否则会导致 npm warn 多次
+      // const pkg = require(path.join(PROJECTS_ROOT, key.substr(1).replace('/', '-'), 'package.json'))
+      // res = { ...deep(pkg.dependencies || {}), ...res }
     }
   }
   return res
