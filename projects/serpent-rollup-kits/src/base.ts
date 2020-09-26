bbb1654bd688e5c3f7b047cf584f1b634ccab63c
朗明 <langming.qzl@antgroup.com>
2020-09-26 13:52:31 +0800

    更新 rollup kits

--- projects/serpent-rollup-kits/src/base.ts
+++ projects/serpent-rollup-kits/src/base.ts
@@ -4,6 +4,8 @@ import findup from 'mora-scripts/libs/fs/findup'
 import replace from '@rollup/plugin-replace'
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
+import alias from '@rollup/plugin-alias'
+import run from '@rollup/plugin-run'
 import json from '@rollup/plugin-json'
 import typescript from '@rollup/plugin-typescript'
 import { terser } from 'rollup-plugin-terser'
@@ -39,24 +41,37 @@ export function customTerser(arg: Parameters<typeof terser>[0] = {}) {
 
 export function getPlugins(
   options: {
-    replace?: Parameters<typeof replace>[0]
-    resolve?: Parameters<typeof resolve>[0]
-    commonjs?: Parameters<typeof commonjs>[0]
-    json?: Parameters<typeof json>[0]
-    typescript?: Parameters<typeof typescript>[0]
+    run?: boolean | Parameters<typeof run>[0]
+    replace?: boolean | Parameters<typeof replace>[0]
+    alias?: boolean | Parameters<typeof alias>[0]
+    resolve?: boolean | Parameters<typeof resolve>[0]
+    commonjs?: boolean | Parameters<typeof commonjs>[0]
+    json?: boolean | Parameters<typeof json>[0]
+    typescript?: boolean | Parameters<typeof typescript>[0]
     terser?: Parameters<typeof terser>[0]
   } = {}
 ) {
+  const getPlugin = <T>(fn: (arg?: T) => any, opts: undefined | boolean | T, defaultOpts?: Partial<T>) => {
+    if (typeof opts === 'boolean') {
+      if (opts === true) return fn(defaultOpts as any)
+    } else {
+      if (defaultOpts) {
+        return fn({ ...defaultOpts, ...opts } as any)
+      }
+      return fn(opts)
+    }
+  }
+
   const normal = [
-    replace(options.replace),
-    resolve({
-      preferBuiltins: true,
-      ...options.resolve,
-    }),
-    commonjs(options.commonjs),
-    json(options.json),
-    typescript({ noEmitOnError: false, ...options.typescript } as any),
-  ]
+    getPlugin(run, options.run == null ? false : options.run), // 默认禁用
+    getPlugin(alias, options.alias == null ? false : options.alias), // 一定要配置 alias 才启用
+    getPlugin(replace, options.replace == null ? false : options.replace), // 一定要配置 replace 才启用
+    getPlugin(resolve, options.resolve, { preferBuiltins: true }),
+    getPlugin(commonjs, options.commonjs),
+    getPlugin(json, options.json),
+    getPlugin(typescript, options.typescript, { noEmitOnError: false }),
+  ].filter(t => !!t)
+
   return {
     normalPlugins: normal,
     minifyPlugins: [...normal, customTerser(options.terser)],
