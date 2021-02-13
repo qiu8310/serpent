9b2e1c6485b8106c9f852a6e25dec511059329fa
Lang Ming <langming.qzl@alibaba-inc.com>
2021-02-13 16:01:37 +0800

    feat(common-cli): makeLib 默认内置生成 modules 脚本

--- projects/serpent-common-cli/rollup.config.js
+++ projects/serpent-common-cli/rollup.config.js
@@ -1,20 +1,3 @@
 import { makeLib } from '@serpent/rollup-kits'
-import exists from 'mora-scripts/libs/fs/exists'
-import path from 'path'
-import fs from 'fs'
 
-const rootDir = path.resolve(__dirname)
-const srcDir = path.join(rootDir, 'src')
-
-const modules = fs.readdirSync(srcDir).reduce((res, k) => {
-  if (k.endsWith('.ts')) {
-    res[k.replace(/\.ts$/, '')] = path.join(srcDir, k)
-  } else if (exists(path.join(srcDir, k, 'index.ts'))) {
-    res[k] = path.join(srcDir, k, 'index.ts')
-  }
-  return res
-}, {})
-
-export default makeLib({
-  modules,
-})
+export default makeLib()
