9381a2e1bc94f12adb321ba6199134cd3b724e65
Lang Ming <langming.qzl@alibaba-inc.com>
2021-02-15 22:48:28 +0800

    update

--- projects/serpent-common-cli/rewrite/typescript.js
+++ projects/serpent-common-cli/rewrite/typescript.js
@@ -5,7 +5,7 @@ Module.prototype.require = function (...args) {
   if (args[0] === 'typescript') {
     // 使用本地的 typescript
     const local = require.resolve(args[0], { paths: [process.cwd()] })
-    return require(local)
+    return originalRequire.apply(this, local)
   }
   return originalRequire.apply(this, args)
 }
