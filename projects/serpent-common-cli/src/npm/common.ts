d2a3aa4db4c998cb201b61d503ba39faf38e055e
Lang Ming <langming.qzl@alibaba-inc.com>
2021-07-25 22:12:19 +0800

    添加getDurkaConfig

--- projects/serpent-common-cli/src/npm/common.ts
+++ projects/serpent-common-cli/src/npm/common.ts
@@ -1,8 +1,23 @@
 import path from 'path'
-import { ospath } from '../fs'
+import { ospath, findupPackage } from '../fs'
 
 export function getDurkaRegistry() {
   return process.env.DURKA_REGISTRY || 'https://registry.npm.taobao.org/'
+}
+
+export function getDurkaConfig() {
+  /* eslint-disable @typescript-eslint/ban-types */
+  type LiteralUnion<T extends U, U = string> = T | (U & {})
+  let res = {} as {
+    language?: LiteralUnion<'typescript' | 'javascript'>
+    env?: LiteralUnion<'node' | 'browser' | 'electron' | 'vscode' | 'any'>
+    framework?: LiteralUnion<'react' | 'vue'>
+    [key: string]: any
+  }
+  try {
+    res = require(findupPackage()).durka || {}
+  } catch (e) {}
+  return res
 }
 
 export function getDurkaHome() {
