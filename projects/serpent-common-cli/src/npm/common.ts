907f5267c1bc2946aa79db162adda53990dea820
Lang Ming <langming.qzl@alibaba-inc.com>
2021-10-17 17:47:21 +0800

    添加 createPackage 函数

--- projects/serpent-common-cli/src/npm/common.ts
+++ projects/serpent-common-cli/src/npm/common.ts
@@ -1,5 +1,5 @@
 import path from 'path'
-import { ospath, findupPackage } from '../fs'
+import { ospath, findupPackage, writeFileSync, toOSPath } from '../fs'
 
 export function getDurkaRegistry() {
   return process.env.DURKA_REGISTRY || 'https://registry.npm.taobao.org/'
@@ -26,4 +26,27 @@ export function getDurkaHome() {
 
 export function getDurkaInstallPrefix() {
   return path.join(getDurkaHome(), 'packages', process.version)
+}
+
+/**
+ * 在指定的目录上生成一个 node 模块，常用于在 durka/vendors 下面注入新模块
+ */
+export function createPackage(
+  nodeModulesDir: string,
+  packageName: string,
+  packageDetail: Record<string, any>,
+  callback: (write: (fileName: string, fileContents: string | string[]) => void, rootDir: string) => void
+) {
+  const packageRoot = path.join(nodeModulesDir, packageName)
+  writeFileSync(
+    path.join(packageRoot, 'package.json'),
+    JSON.stringify({ name: packageName, version: '1.0.0', main: './index.js', ...packageDetail }, null, 2)
+  )
+
+  callback((fileName, fileContents) => {
+    const file = toOSPath(path.join(packageRoot, fileName))
+    const content = Array.isArray(fileContents) ? fileContents.join('\n') : fileContents
+
+    writeFileSync(file, content || '')
+  }, packageRoot)
 }
