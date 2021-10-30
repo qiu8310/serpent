f44d77f0664a15d56b42c05317d4e62d75212f76
Lang Ming <langming.qzl@alibaba-inc.com>
2021-10-30 22:23:41 +0800

    获取远程 package 失败后中的 fallback 提醒置后

--- projects/serpent-common-cli/src/npm/getPackageDetail.ts
+++ projects/serpent-common-cli/src/npm/getPackageDetail.ts
@@ -40,10 +40,10 @@ export async function getPackageDetail(name: string, registry?: string) {
   try {
     return await getRemotePackageDetail(name, registry)
   } catch (e) {
-    logger.warn(` fetch "${name}" error: ${e.message}`)
-    logger.warn(` fallback to offline mode`)
     const detail = await getLocalPackageDetail(name)
     if (!Object.keys(detail.versions).length) throw e // 本地也没有对应的模块
+    logger.warn(` fetch "${name}" error: ${e.message}`)
+    logger.warn(` fallback to offline mode`)
     return detail
   }
 }
