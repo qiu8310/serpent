8ceb004161a1f1937586aa997e9fa84b7a7d1fa8
Lang Ming <langming.qzl@alibaba-inc.com>
2022-06-25 23:37:35 +0800

    feat: 优化 run 函数,支持直接输入命令字符串

--- projects/serpent-common-cli/src/run/index.ts
+++ projects/serpent-common-cli/src/run/index.ts
@@ -29,45 +29,81 @@ interface OutputOptions extends Omit<ChildOptions, 'output'> {
   output: true
 }
 
-export function run(args: string[], options?: ChildOptions): runTypes.ExecaChildProcess
-export function run(args: string[], options?: ChildOptions<null>): runTypes.ExecaChildProcess<Buffer>
-export function run(args: string[], options?: OutputOptions): Promise<string>
-export function run(args: string[], options: any = {}): any {
+/**
+ * 执行某个命令，根据配置项不同，可以返回命令输出的内容，或者子进程
+ * @param args
+ *  - 如果是字符串，会当作 shell 命令执行，如 `runOutput('echo "abc"')`，注意，如果命令中有 shell 相关的功能时，如 "||" 或 "&&"，一定要指定 `shell: true`
+ *  - 如果是数组，会当作普通调用，如 `runOutput(["echo", "abc"])`
+ * @param options
+ *  配置选项
+ */
+export function run(args: string | string[], options?: ChildOptions): runTypes.ExecaChildProcess
+export function run(args: string | string[], options?: ChildOptions<null>): runTypes.ExecaChildProcess<Buffer>
+export function run(args: string | string[], options?: OutputOptions): Promise<string>
+export function run(args: string | string[], options: any = {}): any {
   const { debug, output, ...opts } = options
-
   // 开启 debug 后还需要将环境变量 DURKA_NODE_DEBUG 设置成 true
   if (debug && process.env[typeof debug === 'string' ? debug : 'DURKA_NODE_DEBUG']) {
     return runNodeDebug(args, opts)
   } else if (output) {
     return runOutput(args, opts)
   } else {
-    const [cmd, ...rest] = args
-    if (!opts.silent) log(cmd, rest)
-    return execa(cmd, rest, { stdio: 'inherit', ...opts })
+    if (typeof args === 'string') {
+      if (!opts.silent) log(args)
+      return execa.command(args, { stdio: 'inherit', ...opts })
+    } else {
+      const [cmd, ...rest] = args
+      if (!opts.silent) log(cmd, rest)
+      return execa(cmd, rest, { stdio: 'inherit', ...opts })
+    }
   }
 }
 
-export function runOutput(args: string[], options: runTypes.Options & SilentOptions = {}) {
+/**
+ * 执行某个命令，并返回命令输出的内容
+ * @param args
+ *  - 如果是字符串，会当作 shell 命令执行，如 `runOutput('echo "abc"')`，注意，如果命令中有 shell 相关的功能时，如 "||" 或 "&&"，一定要指定 `shell: true`
+ *  - 如果是数组，会当作普通调用，如 `runOutput(["echo", "abc"])`
+ * @param options
+ *  配置选项
+ */
+export function runOutput(args: string | string[], options: runTypes.Options & SilentOptions = {}) {
   const { silent = true, ...opts } = options
-  const [cmd, ...rest] = args
-  if (!silent) log(cmd, rest)
-  return execa(cmd, rest, { ...opts, stdio: 'pipe' }).then(d => d.stdout)
-}
-
-export function runNodeDebug(args: string[], options: runTypes.Options & SilentOptions = {}) {
-  const { silent, ...opts } = options
-  let [cmd, ...rest] = args
-  if (cmd === 'node') {
-    rest.unshift('--inspect-brk')
+  if (typeof args === 'string') {
+    if (!silent) log(args)
+    return execa.command(args, { ...opts, stdio: 'pipe' }).then(d => d.stdout)
   } else {
-    cmd = 'node'
-    rest = ['--inspect-brk', ...args]
+    const [cmd, ...rest] = args
+    if (!silent) log(cmd, rest)
+    return execa(cmd, rest, { ...opts, stdio: 'pipe' }).then(d => d.stdout)
   }
-  if (!silent) log(cmd, rest)
-  return execa(cmd, rest, { ...opts, stdio: 'inherit' })
 }
 
-function log(cmd: string, args: string[]) {
+export function runNodeDebug(args: string | string[], options: runTypes.Options & SilentOptions = {}) {
+  const { silent, ...opts } = options
+  if (typeof args === 'string') {
+    let cmd = args
+    if (/^\s*node\s/.test(cmd)) {
+      cmd = cmd.replace('node', 'node --inspect-brk')
+    } else {
+      cmd = `node --inspect-brk ${cmd}`
+    }
+    if (!silent) log(cmd)
+    return execa.command(cmd, { ...opts, stdio: 'inherit' })
+  } else {
+    let [cmd, ...rest] = args
+    if (cmd === 'node') {
+      rest.unshift('--inspect-brk')
+    } else {
+      cmd = 'node'
+      rest = ['--inspect-brk', ...args]
+    }
+    if (!silent) log(cmd, rest)
+    return execa(cmd, rest, { ...opts, stdio: 'inherit' })
+  }
+}
+
+function log(cmd: string, args: string[] = []) {
   if (!getBoolEnv('DURKA_SILENT_RUN')) {
     info([cmd, ...args].join(' '))
   }
